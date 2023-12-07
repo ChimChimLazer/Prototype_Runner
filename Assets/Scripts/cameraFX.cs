8367047d18ad64f605f08a374ab64465bf691f43
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2023-12-07 18:09:35 +0000

    Wall run tilt easing

--- Assets/Scripts/cameraFX.cs
+++ Assets/Scripts/cameraFX.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class cameraFX : MonoBehaviour
 {
-    public int WallRunFOVChange;
-
     private float wallRunRotation;
-    public Camera playerCamera;
 
+    [Header("Modifiers")]
+    public float wallRunTiltSpeed;
+    [Header("References")]
+    public GameObject playerCamera;
+
+    private void Start()
+    {
+        wallRunTiltSpeed = wallRunTiltSpeed / 100;
+    }
     void Update()
     {
-        Quaternion cameraTilt = Quaternion.Euler(transform.rotation.x, transform.rotation.y, playerMovement.wallRunCameraRotation);
-        transform.localRotation = cameraTilt;
+        //Quaternion cameraTilt = Quaternion.Euler(transform.rotation.x, transform.rotation.y, playerMovement.wallRunCameraRotation);
+        //transform.localRotation = cameraTilt;
+        
+        if (wallRunRotation != playerMovement.wallRunCameraRotation)
+        {
+            wallRunRotation = playerMovement.wallRunCameraRotation;
+
+            Vector3 cameraTilt = new Vector3(0, 0, wallRunRotation);
+            // Easing was created by using LeanTween by Dented Pixel
+            // https://assetstore.unity.com/packages/tools/animation/leantween-3595#description
+            LeanTween.rotateLocal(playerCamera, cameraTilt, wallRunTiltSpeed);
+        }
     }
 }
