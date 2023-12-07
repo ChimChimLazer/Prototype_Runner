315db7f8d4f5555ccc5f0eeeeaf0024329c43663
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2023-12-07 14:38:36 +0000

    Remove FOV Change

--- Assets/Scripts/cameraFX.cs
+++ Assets/Scripts/cameraFX.cs
@@ -11,29 +11,7 @@ public class cameraFX : MonoBehaviour
 
     void Update()
     {
-        if (wallRunRotation != playerMovement.wallRunCameraRotation)
-        {
-            wallRunRotation = playerMovement.wallRunCameraRotation;
-            setTiltZ(wallRunRotation);
-            if (wallRunRotation != 0)
-            {
-                changeFOV(WallRunFOVChange);
-            } else
-            {
-                changeFOV(-WallRunFOVChange);
-            }
-            
-        }
-    }
-
-    void setTiltZ(float newTilt)
-    {
-        Quaternion cameraTilt = Quaternion.Euler(transform.rotation.x, transform.rotation.y, newTilt);
+        Quaternion cameraTilt = Quaternion.Euler(transform.rotation.x, transform.rotation.y, playerMovement.wallRunCameraRotation);
         transform.localRotation = cameraTilt;
-    }
-
-    void changeFOV(int FOVChange)
-    {
-        playerCamera.fieldOfView += FOVChange;
     }
 }
