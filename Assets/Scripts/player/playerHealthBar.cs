797e2df4d8911ef3c22c33e0f3bcf262002e80f2
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2023-12-29 18:38:27 +0000

    Slider script

--- Assets/Scripts/player/playerHealthBar.cs
+++ Assets/Scripts/player/playerHealthBar.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerHealthBar : MonoBehaviour
 {
+    private Slider healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar = GetComponent<Slider>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
