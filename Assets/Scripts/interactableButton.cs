6882ed07451f704339bc6b65caf3ef27b2b26737
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-01-07 14:07:51 +0000

    interactable Button Animation and Prefab

--- Assets/Scripts/interactableButton.cs
+++ Assets/Scripts/interactableButton.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class interactableButton : MonoBehaviour
 {
     public bool pressed;
-    [SerializeField] Vector3 pressedDepth;
+    [SerializeField] float pressedDepth;
+    [SerializeField] float pressingDepth;
+    [SerializeField] GameObject button;
+
+    private float pressedY;
+    private float unpressedY;
+    private float pressingY;
 
     private void Start()
     {
         pressed = false;
+        unpressedY = button.transform.localPosition.y;
+        pressedY = unpressedY - pressedDepth;
+        pressingY = unpressedY - pressingDepth;
+    }
+    private void Update()
+    {
+        if (button.transform.localPosition.y == pressingY)
+        {
+            pressed = true;
+            LeanTween.moveLocalY(button, pressedY, 0.125f);
+        }
     }
 
     public void press()
     {
-        pressed = true;
-        Debug.Log("Button Pressed");
+        LeanTween.moveLocalY(button, pressingY, 0.25f);
     }
 }
