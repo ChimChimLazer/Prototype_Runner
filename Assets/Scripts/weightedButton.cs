7349bb4af612377ec551aa5a3b614f718435f0f2
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-01-07 13:22:47 +0000

    weighted Button object

---
+++ Assets/Scripts/weightedButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+
+public class weightedButton : MonoBehaviour
+{
+    public bool pressed;
+    void Start()
+    {
+        pressed = false;
+    }
+}
