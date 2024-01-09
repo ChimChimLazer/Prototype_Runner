600306a94501f1880f49e59f1dc8edbe0accc3dc
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-01-09 17:04:54 +0000

    Settings Sliders

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -1,19 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class SettingsMenu : MonoBehaviour
 {
     public GameObject mainMeu;
 
-    public void mainMenu()
+    private float SensX;
+    private float SensY;
+
+    private bool invertX;
+    private bool invertY;
+
+    private float FOV;
+
+
+    public GameObject SensXObject;
+    private Slider SensXSlider;
+    private TextMeshProUGUI SensXTextBox;
+
+    public GameObject SensYObject;
+    private Slider SensYSlider;
+    private TextMeshProUGUI SensYTextBox;
+
+
+    private void Start()
     {
-        mainMeu.SetActive(true); 
-        gameObject.SetActive(false);
+        SensXSlider = SensXObject.GetComponentInChildren<Slider>();
+        SensXTextBox = SensXObject.GetComponentInChildren<TextMeshProUGUI>();
+        SensYSlider = SensYObject.GetComponentInChildren<Slider>();
+        SensYTextBox = SensYObject.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void updateSenx()
+    {
+        SensX = SensXSlider.value;
+        SensXTextBox.text = "Horizontal Sensitivity\n" + (MathF.Round(SensX*10)/10);
+    }
+
+    public void updateSenY()
+    {
+        SensY = SensYSlider.value;
+        SensYTextBox.text = "Vertical Sensitivity\n" + (MathF.Round(SensY * 10) / 10);
     }
 
     public void applySettings()
     {
+        Debug.Log(SensX);
+        Debug.Log(SensY);
 
+        Debug.Log(invertX);
+        Debug.Log(invertY);
+
+        Debug.Log(FOV);
+    }
+
+    public void mainMenu()
+    {
+        mainMeu.SetActive(true); 
+        gameObject.SetActive(false);
     }
 }
