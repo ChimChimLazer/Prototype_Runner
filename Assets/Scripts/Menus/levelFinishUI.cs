8df68cc59999005a83c6226e8b4e0647c365d0b1
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-01-10 14:41:20 +0000

    Level finish UI comments

--- Assets/Scripts/Menus/levelFinishUI.cs
+++ Assets/Scripts/Menus/levelFinishUI.cs
@@ -6,44 +6,53 @@ using UnityEngine.SceneManagement;
 
 public class levelFinishUI : MonoBehaviour
 {
-    public sceneLoader loader;
+    public sceneLoader loader; // sceneLoader that is on the current scene
 
     [Header("Internal Fields")]
-    public TextMeshProUGUI timeText;
-    public TextMeshProUGUI highscoreText;
-    public TextMeshProUGUI levelNameText;
+    public TextMeshProUGUI timeText; // Text box on the UI that displays the time taken to complete the level
+    public TextMeshProUGUI highscoreText; // Text box on the UI that displays the highscore of the current level
+    public TextMeshProUGUI levelNameText; // Text box on the UI that displays the name of the current level
 
-    public GameObject nextLevelButtonObject;
+    public GameObject nextLevelButtonObject; // Button that allows the user to load the next level
 
-    private gameGUI GUI;
+    private gameGUI GUI; // Game GUI that displays the timer
 
+    // Ran on the first frame this object is created
     void Start()
     {
-        GUI = loader.GUI;
+        GUI = loader.GUI; // Gets GUI fromt the loader
 
-        // Update text
+        // Updates the text box's on the UI
         levelNameText.text = SceneManager.GetActiveScene().name;
         timeText.text = ("Time : " + GUI.timerText);
         highscoreText.text = ("Highscore : " + GlobalFunctions.convertTimeToText(loader.highscores[loader.currentScene]));
 
+        // If the current level is the last level hide the next level button
         if (loader.currentScene == loader.scenes.Length - 1)
         {
             nextLevelButtonObject.SetActive(false);
         }
 
+        // Pause the game
         Time.timeScale = 0;
     }
 
+    // Called when the restart button is pressed on the ui
+    // This will reload the current level
     public void restartLevelButton()
     {
         loader.restartScene();
     }
 
+    // Called when the next level button is pressed
+    // This will load the next level
     public void nextLevelButton()
     {
         loader.loadNextScene();
     }
 
+    // Called when the main menu button is pressed
+    // This will load the main menu
     public void mainMenuButton() 
     {
         SceneManager.LoadScene("Main Menu");
