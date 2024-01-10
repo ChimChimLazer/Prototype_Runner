138334985277bd56ad3190293e7a9a9b91882a12
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-01-10 12:15:43 +0000

    HealthBar Colour change and back added

--- Assets/Scripts/player/playerHealthBar.cs
+++ Assets/Scripts/player/playerHealthBar.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class playerHealthBar : MonoBehaviour
 {
     public playerRespawning player;
+    public Image backGround;
     private Slider healthBar;
     private Image bar;
     Color barColor;
+    Color backGroundColor;
 
     void Start()
     {
@@ -16,7 +18,9 @@ public class playerHealthBar : MonoBehaviour
         healthBar.value = healthBar.maxValue;
 
         bar = healthBar.fillRect.GetComponent<Image>();
+
         barColor = bar.color;
+        backGroundColor = backGround.color;
     }
 
     private void Update()
@@ -49,7 +53,11 @@ public class playerHealthBar : MonoBehaviour
             newTrans = Mathf.Lerp(barColor.a, targetTrans, Time.deltaTime * 5);
         }
         
+
         barColor.a = newTrans;
+        backGroundColor.a = newTrans;
+
         bar.color = barColor;
+        backGround.color = backGroundColor;
     }
 }
