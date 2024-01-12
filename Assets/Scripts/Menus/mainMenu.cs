ed38d692ed58dcdf9a4c5c0dcd969066cccfc41b
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-01-12 11:59:38 +0000

    Move debug.log in main menu

--- Assets/Scripts/Menus/mainMenu.cs
+++ Assets/Scripts/Menus/mainMenu.cs
@@ -26,7 +26,7 @@ public class mainMenu : MonoBehaviour
     public void exitGame()
     {
         //https://gamedevbeginner.com/how-to-quit-the-game-in-unity/#:~:text=You%20can%20quit%20a%20game,Play%20Mode%20in%20the%20editor.
-        Debug.Log("Closing Game");
         Application.Quit();
+        Debug.Log("Closed Game");
     }
 }
